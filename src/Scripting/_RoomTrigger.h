3a1b019f023390f8b265e55c3975d2c74380826a
scemino <scemino74@gmail.com>
2019-03-06 18:33:17 +0100

    Call RoomTrigger callbacks on other thread

--- src/Scripting/_RoomTrigger.h
+++ src/Scripting/_RoomTrigger.h
@@ -26,58 +26,78 @@ class _RoomTrigger : public Trigger
             sq_pushobject(_v, _inside);
             SQInteger nparams, nfreevars;
             sq_getclosureinfo(_v, -1, &nparams, &nfreevars);
+
+            std::vector<HSQOBJECT> params;
             if (nparams == 2)
             {
-                sq_pushroottable(_v);
-                sq_pushstring(_v, actor->getName().data(), -1);
-                sq_get(_v, -2);
-                HSQOBJECT actorObject;
-                sq_resetobject(&actorObject);
-                sq_getstackobj(_v, -1, &actorObject);
-                sq_pushobject(_v, _inside);
-                sq_pushobject(_v, _object.getTable());
-                sq_pushobject(_v, actorObject);
+                params.push_back(_inside);
+                params.push_back(_object.getTable());
+                params.push_back(actor->getTable());
             }
             else
             {
-                sq_pushobject(_v, _object.getTable());
+                params.push_back(_inside);
+                params.push_back(_object.getTable());
             }
-            if (SQ_FAILED(sq_call(_v, nparams, SQFalse, SQTrue)))
-            {
-                std::cerr << "failed to call room inside trigger" << std::endl;
-            }
+
+            callTrigger(params, "inside");
         }
         else if (_isInside && !inObjectHotspot)
         {
             _isInside = false;
             if (_outside._type != SQObjectType::OT_NULL)
             {
+                std::vector<HSQOBJECT> params;
+
                 sq_pushobject(_v, _outside);
                 SQInteger nparams, nfreevars;
                 sq_getclosureinfo(_v, -1, &nparams, &nfreevars);
                 if (nparams == 2)
                 {
-                    sq_pushroottable(_v);
-                    sq_pushstring(_v, actor->getName().data(), -1);
-                    sq_get(_v, -2);
-                    HSQOBJECT actorObject;
-                    sq_resetobject(&actorObject);
-                    sq_getstackobj(_v, -1, &actorObject);
                     sq_pushobject(_v, _outside);
                     sq_pushobject(_v, _object.getTable());
-                    sq_pushobject(_v, actorObject);
+                    params.push_back(actor->getTable());
                 }
                 else
                 {
+                    sq_pushobject(_v, _outside);
                     sq_pushobject(_v, _object.getTable());
                 }
 
-                if (SQ_FAILED(sq_call(_v, nparams, SQFalse, SQTrue)))
-                {
-                    std::cerr << "failed to call room outside trigger" << std::endl;
-                }
+                callTrigger(params, "outside");
             }
         }
+    }
+
+    void callTrigger(std::vector<HSQOBJECT> &params, std::string name)
+    {
+        // create thread and store it on the stack
+        auto thread = sq_newthread(_v, 1024);
+        HSQOBJECT thread_obj;
+        sq_resetobject(&thread_obj);
+        if (SQ_FAILED(sq_getstackobj(_v, -1, &thread_obj)))
+        {
+            std::cerr << "Couldn't get coroutine thread from stack" << std::endl;
+            return;
+        }
+
+        for (size_t i = 0; i < params.size(); i++)
+        {
+            sq_pushobject(thread, params[i]);
+        }
+
+        if (SQ_FAILED(sq_call(thread, params.size() - 1, SQFalse, SQTrue)))
+        {
+            std::cerr << "failed to call room " << name << " trigger" << std::endl;
+            return;
+        }
+
+        // create a table for a thread
+        sq_addref(_v, &thread_obj);
+        sq_pushobject(_v, thread_obj);
+
+        std::cout << "start thread: " << thread_obj._unVal.pThread << std::endl;
+        _engine.addThread(thread_obj._unVal.pThread);
     }
 
   private:
