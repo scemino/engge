d6ffc086508a291b983611d10b9fab5175033b21
scemino <scemino74@gmail.com>
2019-08-03 08:35:49 +0200

    Add traces in the log

--- src/Logger.cpp
+++ src/Logger.cpp
@@ -9,12 +9,14 @@ namespace ng
 Logger::Logger()
 {
     auto console_sink = std::make_shared<spdlog::sinks::stdout_color_sink_mt>();
+    console_sink->set_level(spdlog::level::trace);
     auto file_sink = std::make_shared<spdlog::sinks::basic_file_sink_mt>("log.txt", true);
     file_sink->set_level(spdlog::level::trace);
     auto dist_sink = std::make_shared<spdlog::sinks::dist_sink_st>();
     dist_sink->add_sink(console_sink);
     dist_sink->add_sink(file_sink);
     _out = std::make_shared<spdlog::logger>("log", dist_sink);
+    _out->set_level(spdlog::level::trace);
 }
 
 Logger *Locator::_logger{nullptr};
