92be76c4543a4b7320e6649e6682609148350a03
scemino <scemino74@gmail.com>
2019-08-19 16:08:32 +0200

    Allow to read files from filesystem instead of ggpacks

--- include/EngineSettings.h
+++ include/EngineSettings.h
@@ -1,7 +1,5 @@
 #pragma once
-#include <utility>
 #include <string>
-#include <sstream>
 #include "GGPack.h"
 
 namespace ng
@@ -13,41 +11,10 @@ private:
   GGPack _pack2;
 
 public:
-  EngineSettings()
-  {
-    _pack1.open("ThimbleweedPark.ggpack1");
-    _pack2.open("ThimbleweedPark.ggpack2");
-  }
+  EngineSettings();
 
-  bool hasEntry(const std::string &name)
-  {
-    return _pack1.hasEntry(name) || _pack2.hasEntry(name);
-  }
-
-  void readEntry(const std::string &name, std::vector<char> &data)
-  {
-    if (_pack1.hasEntry(name))
-    {
-      _pack1.readEntry(name, data);
-      return;
-    }
-    if (_pack2.hasEntry(name))
-    {
-      _pack2.readEntry(name, data);
-    }
-  }
-
-  void readEntry(const std::string &name, GGPackValue &hash)
-  {
-    if (_pack1.hasEntry(name))
-    {
-      _pack1.readHashEntry(name, hash);
-      return;
-    }
-    if (_pack2.hasEntry(name))
-    {
-      _pack2.readHashEntry(name, hash);
-    }
-  }
+  bool hasEntry(const std::string &name);
+  void readEntry(const std::string &name, std::vector<char> &data);
+  void readEntry(const std::string &name, GGPackValue &hash);
 };
 } // namespace ng
---
+++ src/EngineSettings.cpp
@@ -0,0 +1,65 @@
+#include "EngineSettings.h"
+#include <sstream>
+
+namespace ng
+{
+EngineSettings::EngineSettings()
+{
+    _pack1.open("ThimbleweedPark.ggpack1");
+    _pack2.open("ThimbleweedPark.ggpack2");
+}
+
+bool EngineSettings::hasEntry(const std::string &name)
+{
+    std::ifstream is;
+    is.open(name);
+    if (is.is_open())
+    {
+        is.close();
+        return true;
+    }
+    return _pack1.hasEntry(name) || _pack2.hasEntry(name);
+}
+
+void EngineSettings::readEntry(const std::string &name, std::vector<char> &data)
+{
+    // first try to find the resource in the filesystem
+    std::ifstream is;
+    is.open(name);
+    if (is.is_open())
+    {
+        is.seekg(0, std::ios::end);
+        auto size = is.tellg();
+        data.resize(size);
+        is.seekg(0, std::ios::beg);
+        is.read(data.data(), size);
+        is.close();
+        return;
+    }
+
+    // not found in filesystem, check in the pack files
+    if (_pack1.hasEntry(name))
+    {
+        _pack1.readEntry(name, data);
+        return;
+    }
+
+    if (_pack2.hasEntry(name))
+    {
+        _pack2.readEntry(name, data);
+    }
+}
+
+void EngineSettings::readEntry(const std::string &name, GGPackValue &hash)
+{
+    if (_pack1.hasEntry(name))
+    {
+        _pack1.readHashEntry(name, hash);
+        return;
+    }
+    if (_pack2.hasEntry(name))
+    {
+        _pack2.readHashEntry(name, hash);
+    }
+}
+} // namespace ng
