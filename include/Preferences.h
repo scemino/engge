21bcce0774ed6d5714d224ffc4f7d7c6c1e8d75f
scemino <scemino74@gmail.com>
2019-10-05 16:54:47 +0200

    Add missing header functional

--- include/Preferences.h
+++ include/Preferences.h
@@ -1,8 +1,8 @@
 #pragma once
-#include <string>
-#include <fstream>
-#include <map>
 #include <any>
+#include <functional>
+#include <map>
+#include <string>
 #include "JsonTokenReader.h"
 
 namespace ng
