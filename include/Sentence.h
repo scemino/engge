121957d11264e484cdd87b38ac8f40f66d65a47f
scemino <scemino74@gmail.com>
2019-10-20 13:51:34 +0200

    Add sentence.cpp

--- include/Sentence.h
+++ include/Sentence.h
@@ -8,37 +8,10 @@ namespace ng
 class Sentence : public Function
 {
   public:
-    Sentence &push_back(std::unique_ptr<Function> func)
-    {
-        _functions.push_back(std::move(func));
-        return *this;
-    }
-
-    void stop()
-    {
-        _stopped = true;
-    }
-
-    bool isElapsed() override { return _functions.empty(); }
-
-    void operator()(const sf::Time &elapsed) override
-    {
-        if (_functions.empty())
-            return;
-        if(_stopped)
-        {
-            _functions.clear();
-            return;
-        }
-        if (_functions[0]->isElapsed())
-        {
-            _functions.erase(_functions.begin());
-        }
-        else
-        {
-            (*_functions[0])(elapsed);
-        }
-    }
+    Sentence &push_back(std::unique_ptr<Function> func);
+    void stop();
+    bool isElapsed() override;
+    void operator()(const sf::Time &elapsed) override;
 
   private:
     std::vector<std::unique_ptr<Function>> _functions;
