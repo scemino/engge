db3d919e72bda44953963f5bb93485f17bda7c81
scemino <scemino74@gmail.com>
2020-02-09 18:34:25 +0100

    Be ready for v0.2.0

--- appInfo.cmake
+++ appInfo.cmake
@@ -1,6 +1,6 @@
 set(appName "engge")
 set(appVersionMajor 0)
-set(appVersionMinor 1)
+set(appVersionMinor 2)
 set(appVersionPatch 0)
 set(appVersionSuffix "alpha")
 set(appVersion "${appVersionMajor}.${appVersionMinor}.${appVersionPatch}-${appVersionSuffix}")
