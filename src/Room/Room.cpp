1aead344c1b7a5880cb929a2e617c4a05715fe0d
scemino <scemino74@gmail.com>
2020-02-15 20:33:31 +0100

    Fix invalid animation name
    Fixes #88

--- src/Room/Room.cpp
+++ src/Room/Room.cpp
@@ -241,12 +241,10 @@ struct Room::Impl
             // animations
             if (jObject["animations"].isArray())
             {
-                int animNum = 0;
                 for (auto jAnimation : jObject["animations"].array_value)
                 {
-                    std::ostringstream animName;
-                    animName << "state" << (animNum++);
-                    auto anim = std::make_unique<Animation>(texture, animName.str());
+                    auto animName = jAnimation["name"].string_value;
+                    auto anim = std::make_unique<Animation>(texture, animName);
                     if (!jAnimation["fps"].isNull())
                     {
                         anim->setFps(jAnimation["fps"].int_value);
