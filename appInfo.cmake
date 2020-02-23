1ebfef8de1e5b86011060ef853d039565f216f97
scemino <scemino74@gmail.com>
2020-02-23 20:38:32 +0100

    Bump version

--- appInfo.cmake
+++ appInfo.cmake
@@ -1,6 +1,6 @@
 set(appName "engge")
 set(appVersionMajor 0)
-set(appVersionMinor 2)
+set(appVersionMinor 3)
 set(appVersionPatch 0)
 set(appVersionSuffix "alpha")
 set(appVersion "${appVersionMajor}.${appVersionMinor}.${appVersionPatch}-${appVersionSuffix}")
