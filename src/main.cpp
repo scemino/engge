a9c02b1709d39d25970bcaa063d1e19ca6ace474
scemino <scemino74@gmail.com>
2020-05-08 07:33:01 +0200

    Clean main

--- src/main.cpp
+++ src/main.cpp
@@ -18,78 +18,16 @@
 #include "Audio/SoundManager.hpp"
 #include "Input/DefaultInputEventHandler.hpp"
 #include "Dialog/DialogScriptAbstract.hpp"
+#include "Dialog/_AstDump.hpp"
 namespace fs = std::filesystem;
-
-namespace ng {
-
-class DialogScript : public DialogScriptAbstract {
-public:
-  std::function<bool()> pause(sf::Time time) {
-    std::cout << "pause " << time.asSeconds() << std::endl;
-    return []() { return true; };
-  }
-  std::function<bool()> say(const std::string &actor, const std::string &text) {
-    std::cout << actor << ": " << text << std::endl;
-    return []() { return true; };
-  }
-  void shutup() { std::cout << "shutup" << std::endl; }
-  std::function<bool()> waitFor(const std::string &actor) {
-    std::cout << "waitFor " << actor << std::endl;
-    return []() {
-      return true;
-    };
-  }
-  std::function<bool()> waitWhile(const std::string &condition) {
-    std::cout << "waitWhile " << condition << std::endl;
-    return []() {
-      return true;
-    };
-  }
-  void execute(const std::string &code) {
-    std::cout << "execute " << code << std::endl;
-  }
-  bool executeCondition(const std::string &condition) const {
-    std::cout << "executeCondition " << condition << std::endl;
-    return true;
-  }
-};
-}
 
 int main(int argc, char **argv) {
   if (argc == 2) {
-
     auto filename = argv[1];
     auto filepath = fs::path(filename);
     auto ext = filepath.extension();
-    if (ext.empty()) {
-      ng::Locator<ng::Logger>::create();
-      ng::Locator<ng::EngineSettings>::create().loadPacks();
-      ng::DialogScript ds;
-      ng::DialogPlayer dp(ds);
-      sf::Clock clock;
-      dp.start("ray", filename, "init");
-      while (dp.getState() != ng::DialogManagerState::None) {
-        dp.update();
-      }
-
-      dp.start("ray", filename, "start");
-      while (dp.getState() != ng::DialogManagerState::None) {
-        if (dp.getState() == ng::DialogManagerState::WaitingForChoice) {
-          const auto &choices = dp.getChoices();
-          int i = 1;
-          for (const auto &choice : choices) {
-            if (!choice)
-              continue;
-            auto pChoice = dynamic_cast<ng::Ast::Choice *>(choice->expression.get());
-            std::cout << (i++) << " " << pChoice->text << std::endl;
-          }
-          int choice;
-          std::cin >> choice;
-          dp.choose(choice);
-        }
-        dp.update();
-      }
-      //ng::_AstDump::dump(filename);
+    if (ext == ".byack") {
+      ng::_AstDump::dump(filename);
     } else if (ext == ".save") {
       ng::GGPackValue hash;
       ng::SavegameManager::loadGame(filename, hash);
@@ -119,10 +57,11 @@ int main(int argc, char **argv) {
   }
   catch (std::exception &e) {
     ng::error("Sorry, an error occurred: {}", e.what());
+    return 1;
   }
   catch (...) {
     ng::error("Sorry, an error occurred");
+    return 2;
   }
-
   return 0;
 }
