2be53fab9cd3cd528b2f91f3488bfecbd8a2b020
scemino <scemino74@gmail.com>
2020-12-30 08:35:26 +0100

    Fix load/save game is not drawn correctly

--- src/UI/SaveLoadDialog.cpp
+++ src/UI/SaveLoadDialog.cpp
@@ -69,22 +69,27 @@ struct SaveLoadDialog::Impl {
     inline static const int AutosaveId = 99901;
 
   public:
-    void init(SavegameSlot &slot, SpriteSheet &spriteSheet, Engine &engine) {
+    void init(const SavegameSlot &slot, const SpriteSheet &spriteSheet, Engine &engine) {
       _index = slot.slot - 1;
       auto x = _index % 3;
       auto y = _index / 3;
-      _transform.setPosition({168.f + 39.f * 4.f + 78.f * 4.f * x + 4.f * x,
-                              92.f + 22.f * 4.f + 44.f * 4.f * y + 4.f * y});
+      glm::vec2 pos = {168.f + 39.f * 4.f + 78.f * 4.f * x + 4.f * x,
+                       92.f + 22.f * 4.f + 44.f * 4.f * y + 4.f * y};
+
+      _transform.setPosition(pos);
 
       auto &uiFontSmallBold = engine.getResourceManager().getFntFont("UIFontSmallBold.fnt");
 
+      // prepare the text for the game time
       _gameTimeText.setWideString(slot.getGameTimeString());
       _gameTimeText.setFont(uiFontSmallBold);
       _gameTimeText.setColor(ngf::Colors::White);
-      _gameTimeText.getTransform().setPosition({0.f, -64.f});
+      _gameTimeText.setMaxWidth(600);
       _gameTimeText.setAlignment(ngf::Alignment::Center);
       _gameTimeText.setAnchor(ngf::Anchor::Center);
+      _gameTimeText.getTransform().setPosition({pos.x, pos.y - 64.f});
 
+      // prepare the text for the time when the game has been saved
       std::wstring saveTimeText;
       if (slot.slot == 1) {
         saveTimeText = ng::Engine::getText(AutosaveId);
@@ -94,36 +99,44 @@ struct SaveLoadDialog::Impl {
       _saveTimeText.setWideString(saveTimeText);
       _saveTimeText.setFont(uiFontSmallBold);
       _saveTimeText.setColor(ngf::Colors::White);
-      _saveTimeText.getTransform().setPosition({0.f, 64.f});
+      _saveTimeText.setMaxWidth(600);
       _saveTimeText.setAlignment(ngf::Alignment::Center);
       _saveTimeText.setAnchor(ngf::Anchor::Center);
+      _saveTimeText.getTransform().setPosition({pos.x, pos.y + 64.f});
 
+      // prepare the sprite for the frame
       auto rect = spriteSheet.getRect("saveload_slot_frame");
       _sprite.setTexture(spriteSheet.getTexture());
       _sprite.getTransform().setOrigin({static_cast<float>(rect.getWidth() / 2.f),
                                         static_cast<float>(rect.getHeight() / 2.f)});
       _sprite.getTransform().setScale({4, 4});
       _sprite.setTextureRect(rect);
+      _sprite.getTransform().setPosition(pos);
 
+      // try to find the savegame thumbnail
       std::ostringstream s;
       s << "Savegame" << (_index + 1) << ".png";
 
       _isEmpty = !std::filesystem::exists(s.str());
       if (!_isEmpty) {
+        // prepare a sprite for the savegame thumbnail
         _texture.load(s.str());
         _spriteImg.setTexture(_texture, true);
         _spriteImg.getTransform().setOrigin({160.f, 90.f});
         auto size = _texture.getSize();
         _spriteImg.getTransform().setScale({(rect.getWidth() * 4.f) / size.x, (rect.getHeight() * 4.f) / size.y});
+        _spriteImg.getTransform().setPosition(pos);
         return;
       }
 
+      // or prepare a sprite for the savegame empty slot
       auto saveslotRect = spriteSheet.getRect("saveload_slot");
       _spriteImg.setTexture(spriteSheet.getTexture());
       _spriteImg.setTextureRect(saveslotRect);
       _spriteImg.getTransform().setOrigin({static_cast<float>(saveslotRect.getWidth() / 2.f),
                                            static_cast<float>(saveslotRect.getHeight() / 2.f)});
       _spriteImg.getTransform().setScale({4.f, 4.f});
+      _spriteImg.getTransform().setPosition(pos);
     }
 
     bool contains(const glm::vec2 &pos) const {
@@ -137,7 +150,6 @@ struct SaveLoadDialog::Impl {
     }
 
     void draw(ngf::RenderTarget &target, ngf::RenderStates states) const override {
-      states.transform *= _transform.getTransform();
       _spriteImg.draw(target, states);
       _sprite.draw(target, states);
       if (!_isEmpty) {
@@ -172,7 +184,6 @@ struct SaveLoadDialog::Impl {
 
   void setHeading(bool saveMode) {
     _headingText.setWideString(Engine::getText(saveMode ? SaveGameId : LoadGameId));
-    auto textRect = ng::getGlobalBounds(_headingText);
     _headingText.getTransform().setPosition({Screen::Width / 2.f, 54.f});
     _headingText.setMaxWidth(600);
     _headingText.setAlignment(ngf::Alignment::Center);
@@ -253,7 +264,10 @@ struct SaveLoadDialog::Impl {
 
   void update(const ngf::TimeSpan &) {
     auto pos = _pEngine->getApplication()->getRenderTarget()->mapPixelToCoords(ngf::Mouse::getPosition(),
-      ngf::View(ngf::frect::fromPositionSize({0,0},{Screen::Width,Screen::Height})));
+                                                                               ngf::View(ngf::frect::fromPositionSize({0,
+                                                                                                                       0},
+                                                                                                                      {Screen::Width,
+                                                                                                                       Screen::Height})));
     _backButton.update(pos);
 
     bool isDown = ngf::Mouse::isButtonPressed(ngf::Mouse::Button::Left);
