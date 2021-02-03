7a31d800793c72b5a87952525a1d66c8eea05ccf
scemino <scemino74@gmail.com>
2021-02-03 13:15:07 +0100

    Update version to v0.7.1-beta

--- appInfo.cmake
+++ appInfo.cmake
@@ -1,6 +1,6 @@
 set(appName "engge")
 set(appVersionMajor 0)
 set(appVersionMinor 7)
-set(appVersionPatch 0)
-set(appVersionSuffix "alpha")
+set(appVersionPatch 1)
+set(appVersionSuffix "beta")
 set(appVersion "${appVersionMajor}.${appVersionMinor}.${appVersionPatch}-${appVersionSuffix}")
